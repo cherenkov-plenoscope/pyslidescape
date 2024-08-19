b2b5c865e278fceb20343504fe1fbf0049a7ec50
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-08-19 12:04:27 +0200

    put inkscape related functions into own module

---
+++ pyslidescape/inkscape.py
@@ -0,0 +1,147 @@
+from xml.dom import minidom
+import tempfile
+import os
+import subprocess
+
+
+def inkscape_svg_export_layers(src, dst, hide, show):
+    """
+    Export selected layers of SVG in the file `src` to the file `dst`.
+
+    :arg  str    src:  path of the source SVG file.
+    :arg  str   dst:  path to export SVG file.
+    :arg  list  hide:  layers to hide. each element is a string.
+    :arg  list  show:  layers to show. each element is a string.
+
+    """
+    with open(src, "rt") as f:
+        svg = minidom.parse(f)
+
+    g_hide = []
+    g_show = []
+    for g in svg.getElementsByTagName("g"):
+        if "inkscape:label" in g.attributes:
+            label = g.attributes["inkscape:label"].value
+            if label in hide:
+                g.attributes["style"] = "display:none"
+                g_hide.append(g)
+            elif label in show:
+                g.attributes["style"] = "display:inline"
+                g_show.append(g)
+
+    with open(dst, "wt") as f:
+        f.write(svg.toxml())
+
+    if False:
+        print(
+            "Hide {0} node(s);  Show {1} node(s).".format(
+                len(g_hide), len(g_show)
+            )
+        )
+
+
+def find_inkscape_labels_for_layers_in_inkscape_svg(path):
+    inkscape_labels = []
+    with open(path, "rt") as f:
+        svg = minidom.parse(f)
+
+    for g in svg.getElementsByTagName("g"):
+        if "inkscape:label" in g.attributes:
+            if "id" in g.attributes:
+                if "layer" == g.attributes["id"].value:
+                    inkscape_labels.append(
+                        g.attributes["inkscape:label"].value
+                    )
+    return inkscape_labels
+
+
+def inkscape_svg_roll_out_layers(
+    layers_svg_path,
+    layers_txt_path=None,
+):
+    path_wo_ext, ext = os.path.splitext(layers_svg_path)
+    if layers_txt_path is None:
+        layers_txt_path = path_wo_ext + ".txt"
+
+    dirname = os.path.dirname(layers_svg_path)
+    basename = os.path.basename(layers_svg_path)
+    slide_id_str = str.partition(basename, ".")[0]
+
+    all_labels = set(
+        find_inkscape_labels_for_layers_in_inkscape_svg(path=layers_svg_path)
+    )
+
+    show_label_sets = []
+    with open(layers_txt_path, "rt") as f:
+        lines = f.read()
+        for line in str.splitlines(lines):
+            show_label_sets.append(set(str.split(line, ",")))
+
+    for i in range(len(show_label_sets)):
+        show_label_set = show_label_sets[i]
+        hide_label_set = all_labels.difference(show_label_set)
+        inkscape_svg_export_layers(
+            src=layers_svg_path,
+            dst=os.path.join(
+                dirname, ".{:s}-{:04d}.svg".format(slide_id_str, i)
+            ),
+            show=show_label_set,
+            hide=hide_label_set,
+        )
+
+
+def inkscape_render(svg_path, out_path, background_opacity=0.0):
+    with tempfile.TemporaryDirectory() as tmp:
+        tmp_image_png = os.path.join(tmp, "image.png")
+        rc_ink = subprocess.call(
+            [
+                "inkscape",
+                "--export-background-opacity={:f}".format(background_opacity),
+                "--export-type={:s}".format("png"),
+                "--export-filename={:s}".format(tmp_image_png),
+                svg_path,
+            ]
+        )
+        ext = os.path.splitext(out_path)
+        if ext == ".png":
+            os.rename(tmp_image_png, out_path)
+        else:
+            rc_con = subprocess.call(
+                [
+                    "convert",
+                    tmp_image_png,
+                    "-quality",
+                    "98",
+                    "-background",
+                    "white",
+                    "-alpha",
+                    "remove",
+                    out_path,
+                ]
+            )
+
+
+def init_slide_svg(num_pixel_width, num_pixel_height, docname="slide"):
+    assert num_pixel_width > 0
+    assert num_pixel_height > 0
+
+    svg = """
+    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+    <svg
+       width="{num_pixel_width:d}px"
+       height="{num_pixel_height:d}px"
+       viewBox="0 0 {num_pixel_width:d} {num_pixel_height:d}"
+       version="1.1"
+       id="SVGRoot"
+       inkscape:version="1.2.2 (b0a8486541, 2022-12-01)"
+       sodipodi:docname="{docname:s}"
+       xml:space="preserve"
+       xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+       xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+       xmlns="http://www.w3.org/2000/svg"
+       xmlns:svg="http://www.w3.org/2000/svg">
+    </svg>
+    """
+    return svg
