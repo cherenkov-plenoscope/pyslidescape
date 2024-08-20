66a18b020fbcba9ecff740709088cee8ef2cacbb
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-08-20 12:33:22 +0200

    try to make templates

--- pyslidescape/inkscape.py
+++ pyslidescape/inkscape.py
@@ -86,27 +86,82 @@ def inkscape_render(svg_path, out_path, background_opacity=0.0):
             )
 
 
-def init_slide_svg(num_pixel_width, num_pixel_height, docname="slide"):
+def join_indent(elements):
+    if len(elements) > 0:
+        return "    " + str.join("\n    ", elements) + "\n"
+    else:
+        return ""
+
+
+def init_slide_svg(num_pixel_width, num_pixel_height, elements=[]):
     assert num_pixel_width > 0
     assert num_pixel_height > 0
 
-    svg = """
-    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <!-- Created with Inkscape (http://www.inkscape.org/) -->
-
-    <svg
-        width="{num_pixel_width:d}px"
-        height="{num_pixel_height:d}px"
-        viewBox="0 0 {num_pixel_width:d} {num_pixel_height:d}"
-        version="1.1"
-        id="SVGRoot"
-        inkscape:version="1.2.2 (b0a8486541, 2022-12-01)"
-        sodipodi:docname="{docname:s}"
-        xml:space="preserve"
-        xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-        xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:svg="http://www.w3.org/2000/svg">
-    </svg>
-    """
+    svg = ""
+    svg += '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+    svg += "<!-- Created with pyslidescape (https://github.com/cherenkov-plenoscope/pyslidescape/) -->\n"
+    svg += "\n"
+    svg += "<svg\n"
+    svg += f'    width="{num_pixel_width:d}px"\n'
+    svg += f'    height="{num_pixel_height:d}px"\n'
+    svg += f'    viewBox="0 0 {num_pixel_width:d} {num_pixel_height:d}"\n'
+    svg += '    version="1.1"\n'
+    svg += '    id="SVGRoot"\n'
+    svg += '    xmlns="http:www.w3.org/2000/svg"\n'
+    svg += '    xmlns:svg="http://www.w3.org/2000/svg">\n'
+    svg += join_indent(elements)
+    svg += "</svg>\n"
     return svg
+
+
+def make_text_element(text, x, y, font_size, font_family="Waree", uid=123):
+    return f"""
+    <text
+         xml:space="preserve"
+         style="font-size:{font_size:f}px;font-family:{font_family:s};text-align:center;text-anchor:middle;display:inline;fill:#000000;fill-opacity:0;stroke-width:5"
+         x="{x:f}"
+         y="{y:f}"
+         id="text{uid:d}">
+        {text:s}
+    </text>
+    """
+
+
+def make_rect_element(x, y, dx, dy, uid=123):
+    return f"""
+    <rect
+         style="fill:#000000;stroke-width:5"
+         id="rect{uid:d}"
+         width="{dx:f}"
+         height="{dy:f}"
+         x="{x:f}"
+         y="{y:f}"
+    />
+    """
+
+
+def make_inkscape_layer(label, elements, uid=123):
+    svg = ""
+    svg += "<g\n"
+    svg += '    inkscape:groupmode="layer"\n'
+    svg += f'    id="layer{uid:d}"\n'
+    svg += f'    inkscape:label="{label:s}"\n'
+    svg += '    style="display:inline">\n'
+    svg += join_indent(elements)
+    svg += "</g>\n"
+    return svg
+
+
+def make_image_element(x, y, dx, dy, href, uid=123):
+    return f"""
+    <image
+         width="{dx:f}"
+         height="{dy:f}"
+         preserveAspectRatio="none"
+         xlink:href="{href:s}"
+         id="image{uid:d}"
+         x="{x:f}"
+         y="{y:f}"
+         style="display:inline"
+    />
+    """
